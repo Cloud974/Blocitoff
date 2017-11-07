ccc5be121d3c7acd54b460e952ed6034a0d0e0b5
Michael Fournier <4niermike@gmail.com>
2017-11-07 00:43:46 -0500

    "Added too much at once, scaled back to push commit"

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  #has_many :lists
+  #has_many :collaborators
+
+  def avatar_url(size)
+    gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+
+end
