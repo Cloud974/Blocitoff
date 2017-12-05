534c1feb1c77bd075377387109c60f52cbc6c83f
Michael Fournier <4niermike@gmail.com>
2017-12-05 14:42:00 -0500

    Adding mailer details for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Blocitoff_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'honeydew-4nier.heroku.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address    => "smtp.sendgrid.net",
+    :port       => 25,
+    :user_name  => ENV['SENDGRID_USERNAME'],
+    :password   => ENV['SENDGRID_PASSWORD'],
+    :domain     => ENV['SENDGRID_DOMAIN'],
+    :authentication  => :plain
+}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
